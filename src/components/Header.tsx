2872fdeb696eff1d4f4448dc46c2b9e719dad8d0
Victor Ayogu <victor@seriescode.com>
2019-12-19 11:53:40 +0200

    Switch to bem style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,15 +4,15 @@ import logoWhite from "../statics/img/logo-white.png"
 const Header = ()=> {
     return (
         <header className={"header"}>
-            <div className={"logo-box"}>
-                <img src={logoWhite} alt={""} className={"logo"}/>
+            <div className={"header__logo-box"}>
+                <img src={logoWhite} alt={""} className={"header__logo"}/>
             </div>
-            <div className={"text-box"}>
+            <div className={"header__text-box"}>
                 <h1 className={"heading-primary"}>
-                    <span className={"heading-primary-main"}>Outdoors</span>
-                    <span className={"heading-primary-sub"}> is where life happens</span>
+                    <span className={"heading-primary--main"}>Outdoors</span>
+                    <span className={"heading-primary--sub"}> is where life happens</span>
                 </h1>
-                <a href={"#"} className={"btn btn-white btn-animated"}>Discover Our Tours</a>
+                <a href={"#u"} className={"btn btn--white btn--animated"}>Discover Our Tours</a>
             </div>
         </header>
     )
