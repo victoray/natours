dceb71d6c7e76b4e7cd8f4ab29cf535a9a0cf662
Victor Ayogu <victor@seriescode.com>
2019-12-27 21:14:18 +0200

    Add responsive breakpoints

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,11 +4,11 @@ import nat2 from "../statics/img/nat-2-large.jpg";
 import nat3 from "../statics/img/nat-3-large.jpg";
 
 
-export const About = ()=> {
+export const About = () => {
     return (
         <section className="section-about">
             <div className="u-center-text">
-                <h2 className="heading-secondary u-margin-bottom-big">
+                <h2 className="heading-secondary u-margin-bottom-big u-margin-top-big">
                     Exciting Tours for adventurous people.
                 </h2>
             </div>
@@ -18,9 +18,11 @@ export const About = ()=> {
                         You are going to fall in love with nature.
                     </h3>
                     <p className="paragraph">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore
                         et dolore magna aliqua. Ornare arcu odio ut sem nulla pharetra diam. Purus faucibus ornare
-                        suspendisse sed nisi lacus sed. Nibh venenatis cras sed felis eget velit aliquet sagittis id. Varius sit amet
+                        suspendisse sed nisi lacus sed. Nibh venenatis cras sed felis eget velit aliquet sagittis id.
+                        Varius sit amet
                         mattis vulputate enim.
                     </p>
 
@@ -28,7 +30,8 @@ export const About = ()=> {
                         Live Adventures like you have never had before
                     </h3>
                     <p className="paragraph">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore
                         et dolore magna aliqua.
                     </p>
                     <a href="#" className="btn btn--green">
